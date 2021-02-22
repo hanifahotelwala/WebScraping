1421901f8eb0abae92105f4dfb32bb5faa6433a7
Hanifa Hotelwala <hotelwalahanifa@gmail.com>
2021-02-21 19:57:43 -0600

    Update: movie name + genre + tomato score + audience score successfully scraped from tomatometer.com

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-# Top DVD & Streaming
-# url = "https://www.rottentomatoes.com/browse/top-dvd-streaming?minTomato=0&maxTomato=100&services=amazon;hbo_go;itunes;netflix_iw;vudu;amazon_prime;fandango_now&genres=1;2;4;5;6;8;9;10;11;13;18;14&sortBy=popularity"
-# url = "https://www.rottentomatoes.com/browse/tv-list-2"
-# url = "https://www.rottentomatoes.com/browse/dvd-streaming-all"
-url = "https://www.fandango.com/movie-reviews"
-
 # 2000 top 2000/ 2010 / 2020 movies
-url_00_movies = "https://www.imdb.com/search/title/?year=2000&title_type=feature&"  # top 2000 movies
-url_10_movies = "https://www.imdb.com/search/title/?year=2010&title_type=feature&"  # top 2010 movies
-url_20_movies = "https://www.imdb.com/search/title/?year=2020&title_type=feature&"  # top 2020 movies
+url_00_movies = "https://www.imdb.com/search/title/?year=2000&title_type=feature&"  # top 2000 movies sorted by popularity
+url_10_movies = "https://www.imdb.com/search/title/?year=2010&title_type=feature&"  # top 2010 movies sorted by popularity
+url_20_movies = "https://www.imdb.com/search/title/?year=2020&title_type=feature&"  # top 2020 movies sorted by popularity
 response_00_movies = requests.get(url_00_movies)
 response_10_movies = requests.get(url_10_movies)
 response_20_movies = requests.get(url_20_movies)
@@ -32,13 +26,13 @@ top20movieNames_div = top20moviesContent.select(".lister-item-header a")
 tomatometerUrlMain = "https://www.rottentomatoes.com/m/"
 movieNames_00 = []
 movieURL_00 = []
-movieNames_10 = []  # TODO: 2010
+movieNames_10 = []
 movieURL_10 = []
-movieNames_20 = []  # TODO: 2020
+movieNames_20 = []
 movieURL_20 = []
 
 
-def getStats(url, div):
+def getstats(url, div):
     """
     Scrapes movie page on tomatometer.com and grabs: genre, tomatometer score, and audience score.
     :param url:
@@ -49,7 +43,8 @@ def getStats(url, div):
     getRequest = requests.get(url)
     getContent = BeautifulSoup(getRequest.text, 'html.parser')
     selectContent = getContent.select(div)
-    genre = str(selectContent).split(",")[1]
+    getGenre = getContent.select(".genre , .scoreboard__info")
+    genre = str(getGenre).split(",")[1]  # genre captured
     audienceScore = re.findall("audiencescore=\"(.*?)\"", str(selectContent))  # audience score captured
     tomatoScore = re.findall("tomatometerscore=\"(.*?)\"", str(selectContent))  # tomato score captured
     movieStats.append(genre)
@@ -58,36 +53,70 @@ def getStats(url, div):
     return movieStats
 
 
-## top 10 2000 movies.
-x = 0
-for movies in top00movieNames_div:
-    if x < 10:
-        movies = movies.text
-        movieNames_00.append(movies)
-        ## clean up string and transform into the format for tomatometer  url
-        movieNames_00[x] = movieNames_00[x].lower()
-        if " " in movieNames_00[x]:
-            movieNames_00[x] = movieNames_00[x].replace(" ", "_")
-        if "?" in movieNames_00[x]:
-            movieNames_00[x] = movieNames_00[x].replace("?", "")
-        if "-" in movieNames_00[x]:
-            movieNames_00[x] = movieNames_00[x].replace("-", "")
-        if "," in movieNames_00[x]:
-            movieNames_00[x] = movieNames_00[x].replace(",", "")
+def editmoviename(moviename):
+    """
+    Edits movie name to create url that navigates to tomatometer page for designated movie
+    :param moviename
+    :return: moviename
+    """
+    if " " in moviename:
+        moviename = moviename.replace(" ", "_")
+    if "?" in moviename:
+        moviename = moviename.replace("?", "")
+    if "-" in moviename:
+        moviename = moviename.replace("-", "")
+    if "," in moviename:
+        moviename = moviename.replace(",", "")
+    if "." in moviename:
+        moviename = moviename.replace(".", "")
+    if ":" in moviename:
+        moviename = moviename.replace(":", "")
+    # movie names with a non-uniform url hardcoded.
+    if moviename == "shutter_island":
+        moviename = "1198124-" + moviename
+    if moviename == "kickass":
+        moviename = "1217700-kick_ass"
+    if moviename == "soul":
+        moviename = moviename + "_2020"
+    if moviename == "365_days":
+        moviename = moviename + "_2020"
+    return moviename
 
-        ## append movie name to end of tomatometer url + store in list
-        tomatometerUrl = tomatometerUrlMain + movieNames_00[x]
-        movieURL_00.append(tomatometerUrl)
 
-        print(movieNames_00[x])
-        print(movieURL_00[x])
-        y = getStats(movieURL_00[x], ".scoreboard , .scoreboard__info")
-        ##TODO: wrong genre
-        ## Genre:  Where Art Thou?</h1>\n<p class="scoreboard__info" slot="info">200
-        print("Movie: {} Genre: {} Tomatoscore: {} AudenienceScore: {}".format(movieNames_00[x], y[0], y[1], y[2]))
 
-        tomatometerUrl = tomatometerUrlMain  # reset tomatometer Url
-        x += 1
+def getmovienamesimdb(div, movieNamesArr, movieURLArr,year):
+    """
+    retrieves top 10 popular movies of the following years: 2000, 2010, 2020
+    :param moviename
+    :return: moviename
+    """
+    moviecounter = 0
+    for movies in div:
+        if moviecounter < 10:
+            movies = movies.text
+            movieNamesArr.append(movies)
 
-    else:
-        break
+            movieNamesArr[moviecounter] = movieNamesArr[moviecounter].lower()
+
+            movieNamesArr[moviecounter] = editmoviename(movieNamesArr[moviecounter])
+            ## append movie name to end of tomatometer url + store in list
+            tomatometerUrl = tomatometerUrlMain + movieNamesArr[moviecounter]
+            movieURLArr.append(tomatometerUrl)
+            moviestats = getstats(movieURLArr[moviecounter], ".scoreboard , .scoreboard__info")
+
+            print("{} :: Movie: {} Genre: {} Tomatoscore: {} AudenienceScore: {}".format(year,movieNamesArr[moviecounter],
+                                                                                   moviestats[0], moviestats[1],
+                                                                                   moviestats[2]))
+
+            tomatometerUrl = tomatometerUrlMain  # reset tomatometer Url
+            moviecounter += 1
+
+        else:
+            break
+
+
+
+test = getmovienamesimdb(top00movieNames_div, movieNames_00, movieURL_00, "'00")
+test1 = getmovienamesimdb(top10movieNames_div, movieNames_10, movieURL_10, "'10")
+test2 = getmovienamesimdb(top20movieNames_div, movieNames_20, movieURL_20, "'20")
+
