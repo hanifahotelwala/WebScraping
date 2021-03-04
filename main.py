0a6e3173d54d716d5b78d14b5cc67228dead084c
Hanifa Hotelwala <hotelwalahanifa@gmail.com>
2021-03-04 00:02:57 -0600

    gathers genre count for each year '00,'10,'20 computes averages for tomato score for each genre and audience score for each genre s Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -41,7 +41,9 @@ def getstats(url, div):
     """
     movieStats = []
     getRequest = requests.get(url)
+    # print("getstats response ",getRequest)
     getContent = BeautifulSoup(getRequest.text, 'html.parser')
+
     selectContent = getContent.select(div)
     getGenre = getContent.select(".genre , .scoreboard__info")
     # print(getGenre)
@@ -97,7 +99,6 @@ def getmovienamesimdb(div, movieNamesArr, movieURLArr,year):
     retrieves top 10 popular movies of the following years: 2000, 2010, 2020
     :param moviename
     :return: moviename
-    TODO: PULL 50.
     """
     moviecounter = 0
     datalist = []
@@ -111,6 +112,7 @@ def getmovienamesimdb(div, movieNamesArr, movieURLArr,year):
             movieNamesArr[moviecounter] = editmoviename(movieNamesArr[moviecounter])
             ## append movie name to end of tomatometer url + store in list
             tomatometerUrl = tomatometerUrlMain + movieNamesArr[moviecounter]
+            print(tomatometerUrl)
             movieURLArr.append(tomatometerUrl)
             moviestats = getstats(movieURLArr[moviecounter], ".scoreboard , .scoreboard__info")
 
@@ -122,44 +124,107 @@ def getmovienamesimdb(div, movieNamesArr, movieURLArr,year):
             datalist.append(data)
 
             tomatometerUrl = tomatometerUrlMain  # reset tomatometer Url
+
             moviecounter += 1
 
         else:
             break
     return datalist # data in list.
 
-test = getmovienamesimdb(top00movieNames_div, movieNames_00, movieURL_00, "'00")
-test1 = getmovienamesimdb(top10movieNames_div, movieNames_10, movieURL_10, "'10")
-test2 = getmovienamesimdb(top20movieNames_div, movieNames_20, movieURL_20, "'20")
-
-# print(test[0])
-# print(type(test))
+# test = getmovienamesimdb(top00movieNames_div, movieNames_00, movieURL_00, "'00")
+# test1 = getmovienamesimdb(top10movieNames_div, movieNames_10, movieURL_10, "'10")
+# test2 = getmovienamesimdb(top20movieNames_div, movieNames_20, movieURL_20, "'20")
 
 
-def analyzegenredata(data):
-    genredict = {'Action':0, 'Horror':0, 'Drama':0, 'Romance':0, 'Comedy':0, 'Thriller':0, 'Not Found':0}
+def readlines(year, data):
+    """
+   Analyzes data brought in and computes the following counts: genres, average tomato score, average audience score
+   :param year, data
+   :return: str
+   """
+    genredict = {'Action': 0, 'Horror': 0, 'Drama': 0, 'Romance': 0, 'Comedy': 0, 'Thriller': 0, 'Not Found': 0}
+    avgTomatoScore = {'Action': 0, 'Horror': 0, 'Drama': 0, 'Romance': 0, 'Comedy': 0, 'Thriller': 0}
+    avgAudienceScore = {'Action': 0, 'Horror': 0, 'Drama': 0, 'Romance': 0, 'Comedy': 0, 'Thriller': 0}
+    genrePattern = "Genre:(.*?)Tomatoscore"
+    tomatoScorePattern = "Tomatoscore:\s\[\'(.*?)\'"
+    audienceScorePattern = "AudenienceScore:\s\[\'(.*?)\'"
 
-    for x in data:
-        genrePattern = "Genre:(.*?)Tomatoscore"
-        substring = re.search(genrePattern,x).group(1).replace(" ","").lower()
-        if substring.find("action"):
-            genredict["Action"] += 1
-        if substring.find("horror"):
-            genredict["Horror"] += 1
-        if substring.find("drama"):
-            genredict["Drama"] += 1
-        if substring.find("romance"):
-            genredict["Romance"] += 1
-        if substring.find("comedy"):
-            genredict["Comedy"] += 1
-        if substring.find("thriller"):
-            genredict["Thriller"] += 1
-        if substring.find("not found"):
-            genredict["Not Found"] +=1
+    for line in data:
+        if line.startswith(year):
+            if re.search(tomatoScorePattern,line) is not None and re.search(tomatoScorePattern,line).group(1) is not '':
+                #add up all of the scores for each: tomato score  and audience score
+                if "Horror" in line:
+                    ht = float(re.search(tomatoScorePattern,line).group(1))
+                    ha = float(re.search(audienceScorePattern,line).group(1))
+                    avgTomatoScore["Horror"] += ht
+                    avgAudienceScore["Horror"] += ha
+                if "Drama" in line:
+                    dt = float(re.search(tomatoScorePattern,line).group(1))
+                    da = float(re.search(audienceScorePattern,line).group(1))
+                    avgTomatoScore["Drama"] += dt
+                    avgAudienceScore["Drama"] += da
+                if "Action" in line:
+                    at = float(re.search(tomatoScorePattern,line).group(1))
+                    aa = float(re.search(audienceScorePattern,line).group(1))
+                    avgTomatoScore["Action"] += at
+                    avgAudienceScore["Action"] += aa
+                if "Romance" in line:
+                    rt = float(re.search(tomatoScorePattern,line).group(1))
+                    ra = float(re.search(audienceScorePattern,line).group(1))
+                    avgTomatoScore["Romance"] += rt
+                    avgAudienceScore["Romance"] += ra
+                if "Comedy" in line:
+                    ct = float(re.search(tomatoScorePattern,line).group(1))
+                    ca = float(re.search(audienceScorePattern,line).group(1))
+                    avgTomatoScore["Comedy"] += ct
+                    avgAudienceScore["Comedy"] += ca
+                if "thriller" in line:
+                    tt = float(re.search(tomatoScorePattern,line).group(1))
+                    ta = float(re.search(audienceScorePattern,line).group(1))
+                    avgTomatoScore["Thriller"] += tt
+                    avgAudienceScore["Thriller"] += ta
+            # get genre counts
+            substring = re.search(genrePattern, line).group(1).replace(" ", "").lower()
+            genredict["Action"] += substring.count("action")
+            genredict["Horror"] += substring.count("horror")
+            genredict["Drama"] += substring.count("drama")
+            genredict["Romance"] += substring.count("romance")
+            genredict["Comedy"] += substring.count("comedy")
+            genredict["Thriller"] += substring.count("thriller")
+            genredict["Not Found"] += substring.count("notfound")
 
-    return genredict
+    #compute averages
+    avgTomatoScore["Horror"] = avgTomatoScore["Horror"] / genredict["Horror"]
+    avgAudienceScore["Horror"] = avgTomatoScore["Horror"] /genredict["Horror"]
+    avgTomatoScore["Drama"] = avgTomatoScore["Drama"] / genredict["Drama"]
+    avgAudienceScore["Drama"] = avgTomatoScore["Drama"] / genredict["Drama"]
+    avgTomatoScore["Action"] = avgTomatoScore["Action"] / genredict["Action"]
+    avgAudienceScore["Action"] = avgTomatoScore["Action"] / genredict["Action"]
+    avgTomatoScore["Romance"] = avgTomatoScore["Romance"] / genredict["Romance"]
+    avgAudienceScore["Romance"] = avgTomatoScore["Romance"] / genredict["Romance"]
+    avgTomatoScore["Comedy"] = avgTomatoScore["Comedy"] / genredict["Comedy"]
+    avgAudienceScore["Comedy"] = avgTomatoScore["Comedy"] / genredict["Comedy"]
+    avgTomatoScore["Thriller"] = avgTomatoScore["Thriller"] / genredict["Thriller"]
+    avgAudienceScore["Thriller"] = avgTomatoScore["Thriller"] / genredict["Thriller"]
 
-# print(analyzedata("Genre:  Comedy/Horror/Action Tomatoscore: ['70'] AudenienceScore: ['85']"))
-print("2000 Genres",analyzegenredata(test))
-print("2010 Genres",analyzegenredata(test1))
-print("2020 Genres",analyzegenredata(test2))
+
+    print year
+    print "Genre Count: ",genredict
+    print "Avg Tomato Score: ",avgTomatoScore
+    print "Avg Audience Score: ",avgAudienceScore
+
+
+
+def readfile():
+    """
+   Reads txt file that has data from scraped website
+   """
+    file = open("/Users/hanifahotelwala/Desktop/rottendata.txt")
+    data = file.readlines()
+    readlines("'00",data)
+    readlines("'10",data)
+    readlines("'20",data)
+
+    file.close()
+
+readfile()
